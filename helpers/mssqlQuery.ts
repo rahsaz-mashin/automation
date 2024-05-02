6583e8fdb9fc1b2b1ac92857be110cd61d963728
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-05-02 15:36:22 +0330

    fixed mssql issue in credential

--- helpers/mssqlQuery.ts
+++ helpers/mssqlQuery.ts
@@ -1,25 +1,30 @@
-import sql from "mssql"
+import mssql from "mssql"
 import {ICredentialDataDecryptedObject} from "n8n-workflow";
 
 export const MssqlQuery = async (credential: ICredentialDataDecryptedObject, query: string) => {
-	const sqlConfig = {
+
+	const config = {
 		server: credential.server as string,
+		port: credential.port as number,
+		database: credential.database as string,
 		user: credential.user as string,
 		password: credential.password as string,
-		database: credential.database as string,
-		port: credential.port as number,
-		pool: {
-			max: 10,
-			min: 0,
-			idleTimeoutMillis: 30000
-		},
+		domain: credential.domain ? (credential.domain as string) : undefined,
+		connectionTimeout: credential.connectTimeout as number,
+		requestTimeout: credential.requestTimeout as number,
 		options: {
-			encrypt: true,
-			trustServerCertificate: true
-		}
-	}
-	await sql.connect(sqlConfig)
-	return (await sql.query(query)).recordset
+			encrypt: credential.tls as boolean,
+			enableArithAbort: false,
+			tdsVersion: credential.tdsVersion as string,
+			trustServerCertificate: credential.allowUnauthorizedCerts as boolean,
+		},
+	};
+
+	const pool = new mssql.ConnectionPool(config);
+	await pool.connect();
+
+	const request = pool.request();
+	return (await request.query(query)).recordset
 }
 
 
